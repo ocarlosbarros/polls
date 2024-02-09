035f2b0cb6adc9fc42e0d319f3a1ab64b746d7c6
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-08 23:10:33 -0300

    feat(server):adds status code 201 using reply

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,7 +6,7 @@ const app = fastify();
 
 const prisma = new PrismaClient();
 
-app.post("/polls", async (request) => {
+app.post("/polls", async (request, reply) => {
     const createPollBody = z.object({
         title: z.string()
     });
@@ -19,7 +19,7 @@ app.post("/polls", async (request) => {
         }
     });
 
-    return { pollId: poll.id };
+    return reply.status(201).send({ pollId: poll.id });
 });
 
 app.listen({ port: 3333}).then(() => {
