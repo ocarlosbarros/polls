ccec2b972515bb4d5bba8a60b7fe0c48059146b7
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-10 23:15:40 -0300

    feat(VoteOnPollRepository):creates a create vote method

--- src/repositories/VoteOnPollRepository.ts
+++ src/repositories/VoteOnPollRepository.ts
@@ -1,5 +1,17 @@
 class VoteOnPollRepository {
 
+    public create = async (vote: any ) => {
+
+        const created = await prisma.vote.create({
+            data: {
+                sessionId: vote.sessionId,
+                pollId: vote.pollId,
+                pollOptionId: vote.pollOptionId
+            }
+        });
+
+        return created;
+    }
 }
 
 export default VoteOnPollRepository;
