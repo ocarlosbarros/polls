de1830455693b0c61c584a702eb1d0c7368c771d
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-10 21:01:17 -0300

    fet(voteOnPoll):creates a new route

---
+++ src/routes/voteOnPoll.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+import VoteOnPollController from "../controllers/VoteOnPollController";
+import VoteOnPollRepository from "../repositories/VoteOnPollRepository";
+
+export default async function createPoll(app: FastifyInstance){
+    const voteOnPollRepository = new VoteOnPollRepository;
+    const voteOnPollController = new VoteOnPollController(voteOnPollRepository);
+    
+    app.post('/polls/:pollId/votes', voteOnPollController.create)
+
+}
